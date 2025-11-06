4f6332f44b5e9b6f4d38dce454ee84124dc278a9
Ajmal Nasumudeen <ajmaln73@gmail.com>
2025-11-06 20:43:16 +0530

    ok

--- api/server.js
+++ api/server.js
@@ -8,7 +8,12 @@ import appRoutes from './src/routes/index.js';
 
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",        
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
+  })
+);
 app.use(express.json());
 app.use(morgan('dev'));
 
