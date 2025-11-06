60504288da7fa8db5f54104f5636f6560e40dfa1
Ajmal Nasumudeen <ajmaln73@gmail.com>
2025-11-06 21:01:40 +0530

    ok

--- api/src/data/farmers.js
+++ api/src/data/farmers.js
@@ -1,5 +1,6 @@
 export const farmers = [
   { name: 'John Smith', email: 'Pbfgmarketplace@gmail.com', product: 'potato' },
   { name: 'Maria Garcia', email: 'Pbfgmarketplace@gmail.com', product: 'tomato' },
-  { name: 'David Chen', email: 'Pbfgmarketplace@gmail.com', product: 'wheat' }
+  { name: 'David Chen', email: 'Pbfgmarketplace@gmail.com', product: 'wheat' },
+   { name: 'Ajmal', email: 'ajmaln73@gmail.com', product: 'test' }
 ];
